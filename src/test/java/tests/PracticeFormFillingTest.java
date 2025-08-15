9621571f1a876ddb4098242aa5932455c89f066d
Dmitriy Sim <osim57@mail.ru>
2025-08-15 16:45:04 +0500

    Edits after code review

--- src/test/java/tests/PracticeFormFillingTest.java
+++ src/test/java/tests/PracticeFormFillingTest.java
@@ -6,13 +6,12 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormFillingTest {
 
     @BeforeAll
-    static void beforeAll() {
+    static void prepareEnvironment() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
@@ -23,6 +22,8 @@ public class PracticeFormFillingTest {
     void fillFieldAndCheckOutput () {
         //Открытие тестируемой страницы
         open("/automation-practice-form");
+        executeJavaScript("$('footer').remove();");
+        executeJavaScript("$('#fixedban').remove();");
 
         //Заполнение текстовых полей
         $("#firstName").sendKeys("Homer");
@@ -34,9 +35,9 @@ public class PracticeFormFillingTest {
 
         //Выбор значения для датапикера
         $("#dateOfBirthInput").click();
-        $("[class=react-datepicker__month-select]").click();
+        $(".react-datepicker__month-select").click();
         $(by("value", "4")).click();
-        $("[class=react-datepicker__year-select]").click();
+        $(".react-datepicker__year-select").click();
         $(by("value", "1988")).click();
         $(".react-datepicker__day--025").click();
 
@@ -50,9 +51,9 @@ public class PracticeFormFillingTest {
         $("#uploadPicture").uploadFromClasspath("oldmanyellstosky.jpg");
 
         //Выбор значений в выпадающем списке
-        $(byXpath("//*[contains(text(), 'Select State')]")).click();
+        $("#state").click();
         $("#react-select-3-option-3").click();
-        $(byXpath("//*[contains(text(), 'Select City')]")).click();
+        $("#city").click();
         $("#react-select-4-option-0").click();
 
         //Передача данных
