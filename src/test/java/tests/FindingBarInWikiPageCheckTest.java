1b9ab421f549cb393dd8b3bd553dd649201f9075
Dmitriy Sim <osim57@mail.ru>
2025-08-16 22:33:52 +0500

    Edits after CR

--- src/test/java/tests/FindingBarInWikiPageCheckTest.java
+++ src/test/java/tests/FindingBarInWikiPageCheckTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,10 +7,20 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.withTagAndText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Тест проверки блока фильтрации на странице библиотеки в GH и проверки наличия кода на странице раздела Wiki")
 public class FindingBarInWikiPageCheckTest {
+    private final String codeExampleForJUnit5 = """
+            @ExtendWith({SoftAssertsExtension.class})
+            class Tests {
+              @Test
+              void test() {
+                Configuration.assertionMode = SOFT;
+                open("page.html");
+            
+                $("#first").should(visible).click();
+                $("#second").should(visible).click();
+              }""";
 
     /**
      * Запуск браузера и открытие стартовой страницы в GH,
@@ -35,10 +44,11 @@ public class FindingBarInWikiPageCheckTest {
         $("#wiki-tab").shouldBe(visible).click();
 
         $("#wiki-pages-filter").sendKeys("soft");
-        String wikiURL = Selenide.webdriver().driver().url();
-        assertTrue(wikiURL.contains("selenide/wiki"), "Открыта страница с несоответствующим URL");
-
-        $$(".Box-row.px-2.py-2").get(17).$("a").shouldHave(exactTextCaseSensitive("SoftAssertions")).click();
-        $(withTagAndText("h4","JUnit5" )).parent().sibling(0).shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+        $$("[class$=py-2]")
+                .findBy(text("SoftAssertions"))
+                .shouldBe(visible)
+                .$("a")
+                .click();
+        $(withTagAndText("h4", "JUnit5")).parent().sibling(0).shouldHave(text(codeExampleForJUnit5));
     }
 }
